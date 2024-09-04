06713931d10e5d4179fa3e701114dcae793198eb
Rabia Basharat <rabiabasharat22@gmail.com>
2024-09-04 02:26:07 -0700

    update

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>home page<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/home-pic1.jpg" alt="" width="384" height="512"/>
+    <div>address page<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-         
-        </p>
-      </blockquote>
+        Company/Organization Name: G-Tech
+
+        </p>Country: Pakistan
+
+      </blockquote>City: Rahim Yar Khan
+
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          
+        Province/Region: Punjab
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          
+        Address: Gulshan-e-Iqbal , RYK
         </div>
       </figcaption>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,21 +2,25 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>home page<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/home-pic1.jpg" alt="" width="384" height="512"/>
+    <div>contact us page<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-         
+        Company/Organization Name: G-tech
+
+
+       
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          
+        Address: Gulshan-e-Iqbal , RYK 
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          
-        </div>
+        Phone Number: 0310-0664728   
+        </div>Email Address: rabiabasharat22gmail.com
+
       </figcaption>
     </div>
   </figure></div>
